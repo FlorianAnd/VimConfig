83def7c6e6465ff712eed10c282b1a62202fa893
florian <florian.andrieux@sayens.fr>
2019-11-04 15:32:59 +0100

    fix emmet and repl

---
+++ .vim_runtime/my_plugins/vim-repl/test/test.py
@@ -0,0 +1,8 @@
+def f(a, b):
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+    print("hello")
+
+f(range(4), range(3))
---
+++ .vim_runtime/my_plugins/vim-repl/test/testnumpy.py
@@ -0,0 +1,3 @@
+import numpy as np
+a_variable = np.zeros(19)
+b_variable = np.ones(10)
